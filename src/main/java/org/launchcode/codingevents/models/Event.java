128dd67691f9264c3ebead8118fa40c062b5cfcc
clairepatt19@gmail.com <clairepatt19@gmail.com>
2021-06-24 12:38:39 -0500

    initial commit

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -85,6 +85,7 @@ public class Event {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
         Event event = (Event) o;
         return id == event.id;
     }
