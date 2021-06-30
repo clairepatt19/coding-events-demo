912857b1ffd43086dc3043b01c7d08ffbf218eea
clairepatt19@gmail.com <clairepatt19@gmail.com>
2021-06-29 22:29:54 -0500

    final updates working!

--- src/main/java/org/launchcode/codingevents/data/EventCategoryRepository.java
+++ src/main/java/org/launchcode/codingevents/data/EventCategoryRepository.java
@@ -1,10 +1,11 @@
 package org.launchcode.codingevents.data;
 
+import jdk.jfr.Category;
 import org.launchcode.codingevents.models.Event;
+import org.launchcode.codingevents.models.EventCategory;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventCategoryRepository extends CrudRepository<Event,Integer> {
-
+public interface EventCategoryRepository extends CrudRepository<EventCategory,Integer> {
 }
